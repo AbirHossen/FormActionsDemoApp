a33b194ee54c55d603a6da134c6448bb756cab0a
Abir Hossen <abirhossen16@gmail.com>
2025-11-23 16:57:32 +0600

    validating inputs using useActionState

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,34 @@
+import { useActionState } from "react";
 export function NewOpinion() {
+  function submitAction(prevFormState, formData) {
+    const name = formData.get("userName");
+    const title = formData.get("title");
+    const opinion = formData.get("body");
+    const errors = [];
+    if (!name) {
+      errors.push("Please enter your name.");
+    }
+    if (!title) {
+      errors.push("Please enter a title.");
+    }
+    if (!opinion) {
+      errors.push("Please enter your opinion.");
+    }
+    if (errors.length > 0) {
+      return { errors, enteredValues: { name, title, opinion } };
+    }
+
+    return { errors: null };
+  }
+
+  const [formState, formAction] = useActionState(submitAction, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
@@ -18,6 +44,15 @@ export function NewOpinion() {
           <label htmlFor="body">Your Opinion</label>
           <textarea id="body" name="body" rows={5}></textarea>
         </p>
+        <div>
+          {formState.errors && (
+            <ul>
+              {formState.errors.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )}
+        </div>
 
         <p className="actions">
           <button type="submit">Submit</button>
